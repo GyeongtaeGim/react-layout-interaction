7080c32d097fbc5a3e52296edba7b74128df4ad5
Gyeongtae Gim <elnotlu@gmail.com>
2024-01-21 22:20:30 +0900

    ✨ LinkButton 추가

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,10 +2,19 @@ import classNames from 'classnames';
 import { ComponentProps, PropsWithoutRef, forwardRef } from 'react';
 
 import styles from './styles.module.css';
+import { Link, LinkProps } from 'react-router-dom';
 
-export interface ButtonProps extends PropsWithoutRef<ComponentProps<'button'>> {
+export interface BaseButtonProps {
   color?: 'grey' | 'blue';
 }
+
+export interface ButtonProps
+  extends Omit<PropsWithoutRef<ComponentProps<'button'>>, 'color'>,
+    BaseButtonProps {}
+
+export interface LinkButtonProps
+  extends Omit<LinkProps, 'color'>,
+    BaseButtonProps {}
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, color = 'grey', ...props }, forwardedRef) => {
@@ -19,4 +28,20 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   },
 );
 
+Button.displayName = 'button';
+
 export default Button;
+
+export const LinkButton = forwardRef<HTMLAnchorElement, LinkButtonProps>(
+  ({ className, color = 'grey', ...props }, forwardedRef) => {
+    return (
+      <Link
+        className={classNames(styles['button'], styles[color], className)}
+        {...props}
+        ref={forwardedRef}
+      />
+    );
+  },
+);
+
+LinkButton.displayName = 'LinkButton';
